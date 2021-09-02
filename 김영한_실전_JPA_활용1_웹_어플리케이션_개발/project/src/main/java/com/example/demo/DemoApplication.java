bead9e8f6523dceee90d2ae9c65060cd4bf44b5c
evan.h <evan.h@kakaocorp.com>
2021-09-03 00:41:27 +0900

    김영한 JPA 스터디 시작

---
+++ 김영한_실전_JPA_활용1_웹_어플리케이션_개발/project/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import com.example.demo.domain.Order;
+import com.example.demo.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
